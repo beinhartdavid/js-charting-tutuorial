9fcd482020a76d66d1f786350693b26f36389b84
beinhartdavid <beinhart.david@gmail.com>
2020-01-16 22:34:49 -0500

    Step2 Done

---
+++ js/index.js
@@ -0,0 +1,11 @@
+JSC.Chart('chartDiv', {
+    type: 'column',
+    series: [
+       {
+          points: [
+             {x: 'Apples', y: 50},
+             {x: 'Oranges', y: 42}
+          ]
+       }
+    ]
+ });
